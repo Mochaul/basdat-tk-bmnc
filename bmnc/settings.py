ca0ba5384bbe810fd7d5323d04fd36328e86248c
Norman <normanbintang@hotmail.co.id>
2018-05-20 11:45:37 +0700

    add default setting

--- bmnc/settings.py
+++ bmnc/settings.py
@@ -78,6 +78,16 @@ WSGI_APPLICATION = 'bmnc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# #If Using Heroku Environemnt, then Use Database Setting on Heroku
+# if PRODUCTION:
+#     DATABASES['default'] = dj_database_url.config()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
