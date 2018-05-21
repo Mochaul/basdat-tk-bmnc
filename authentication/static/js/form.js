47aecf3ced06725c597971c976f2caacfb8132ff
Mochamad Aulia <praditomo@gmail.com>
2018-05-21 10:42:13 +0700

    progress on registration

--- authentication/static/js/form.js
+++ authentication/static/js/form.js
@@ -1,10 +1,13 @@
-function checkRole(selected) {
-    var val = document.selected.value;
-    if (val === "mahasiswa") {
-        document.getElementById("status-kemahasiswaan").style.display = 'inline';
-        document.getElementById("id-universitas").style.display = 'none';
-    } else{
-        document.getElementById("status-kemahasiswaan").style.display = 'none';
-        document.getElementById("id-universitas").style.display = 'inline';
+$("#password").change(function(){
+    var pass=$(this).val();
+    if(^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$.test(pass)){
+      alert("Your password must be at least 8 characters long and contain alphanumeric. Please try another.")
     }
-}
+  });
+
+$("#nomor-id").change(function(){
+    var no=$(this).val();
+    if(no.length!=10){
+      alert("NPM/NIK you entered must be 10 characters long.")
+    }
+  });
--- authentication/views.py
+++ authentication/views.py
@@ -14,3 +14,18 @@ def login(request):
 def register(request):
     html = 'register.html'
     return render(request, html, response)
+
+def register_user(request, url_berita):
+    id = Komentar.objects.last().id + 1
+    tanggal = datetime.now()
+    jam = datetime.time(datetime.now())
+    konten = request.POST.get('konten')
+    nama_user = request.POST.get('nama_user')
+    email_user = request.POST.get('email_user')
+    url_user = request.POST.get('url_user')
+
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO komentar VALUES (%d, '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % (id, tanggal, jam, konten, nama_user, email_user, url_user, url_berita)
+        cursor.execute(sql)
+
+    return HttpResponseRedirect(reverse('berita:detail-berita', kwargs={'url_berita': url_berita}))
