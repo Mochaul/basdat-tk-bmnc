a134628ea3258ab48f06ea62435f4d1832be561a
Mochamad Aulia <praditomo@gmail.com>
2018-05-21 14:35:55 +0700

    progress on login

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import login, register, register_user
+from .views import login, register, register_user, authenticate_user
 
 urlpatterns = [
     url(r'^login/$', login, name='login'),
+    url(r'^login/authenticate/$', authenticate_user, name='authenticate-user'),
     url(r'^register/$', register, name='register'),
     url(r'^register/register-user/$', register_user, name='register-user'),
 ]
